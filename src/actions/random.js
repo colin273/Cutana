56f9cd99bcd37cdbf66e56b7c8d81df646d2be36
FifiTheBulldog <fifithebulldog@outlook.com>
2021-06-26 15:11:47 -0400

    Better rounding logic for Random Number action

--- src/actions/random.js
+++ src/actions/random.js
@@ -1,17 +1,23 @@
 const Action = require("../types/actions/action.js");
 
+function roundFloat(x) {
+	const float = parseFloat(x)
+	return (float > 0) ? Math.floor(float) : Math.ceil(float)
+}
+
 class RandomAction extends Action {
 	run() {
-		const min = Math.floor(parseFloat(this.parameters.randomNumberMinimum));
-		const max = Math.floor(parseFloat(this.parameters.randomNumberMaximum));
+		const min = roundFloat(this.parameters.randomNumberMinimum);
+		const max = roundFloat(this.parameters.randomNumberMaximum);
 
 		if (min > max) {
 			return null;
 		}
 
-		return Math.floor(Math.random() * (max + 1 - min)) + min
+		return Math.floor(Math.random() * (max + 1 - min)) + min;
 	}
 }
+
 RandomAction.identifier = "is.workflow.actions.number.random";
 
 module.exports = RandomAction;
